768e157f6d8a75ac5d8466953a58f2848e2db40f
Javier Negre <javi.negre@gmail.com>
2017-05-29 15:59:07 +0200

    Add CV

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express'),
-  app = express();
+  app = express(),
+  path = require('path');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get(/^\/(des\/)?$/, (req, res) => {
   res.send(`This is the ${req.url} route in home`);
+});
+
+app.get('/cv', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/cv.html'));
 });
 
 app.get('*', (req, res) => {
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,15 @@ import CVChart from './cv/CVChart';
 import '../scss/style.scss';
 
 if (document.querySelector('svg#cv-chart') !== null) {
+
+  const host = 'negre.co';
+  const name = 'javi';
+  const emailLink = `mailto:${name}@${host}`;
+
+  for (const el of document.getElementsByClassName('js-mailto')) {
+    el.setAttribute('href', emailLink);
+  }
+
   const chart = new CVChart('svg#cv-chart');
   chart.render();
 }
