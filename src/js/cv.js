11685b3110e916f63d625ef00db3700c64d00bff
Javier Negre <javi.negre@gmail.com>
2019-04-13 12:39:37 +0200

    Minor content changes

--- src/js/cv.js
+++ src/js/cv.js
@@ -3,29 +3,6 @@ import 'waypoints/lib/noframework.waypoints';
 import CVChart from './cv/CVChart';
 
 const CV = () => {
-  const host = 'gmail.com';
-  const name = 'javi.negre';
-  const emailLink = `mailto:${name}@${host}`;
-  const mailtos = document.getElementsByClassName('js-mailto');
-  const len = mailtos.length;
-  for (let i = 0; i < len; i += 1) {
-    mailtos[i].setAttribute('href', emailLink);
-  }
-
-  const $birth = document.querySelector('[data-birthdate]');
-  const birth = new Date($birth.dataset.birthdate);
-  const now = new Date();
-  let age = now.getFullYear() - birth.getFullYear();
-
-  const strNow = `${now.getMonth()}-${now.getDate()}`;
-  const strBirth = `${birth.getMonth()}-${birth.getDate()}`;
-
-  if (strNow < strBirth) {
-    age -= 1;
-  }
-
-  $birth.innerHTML = age;
-
   const cvChart = new CVChart('#cv-chart');
   cvChart.draw();
 
