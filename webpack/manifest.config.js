8070e8ff241f1ef4a6e149ad623aace61e93418d
Javier Negre <javi.negre@gmail.com>
2019-07-05 17:22:53 +0200

    Fix icon src paths in manifest file

--- webpack/manifest.config.js
+++ webpack/manifest.config.js
@@ -9,17 +9,17 @@ module.exports = {
     'background_color': '#000000',
     'icons': [
         {
-            'src': path.resolve('src/assets/img/favicon-color-144.png'),
+            'src': '/assets/img/favicon-color-144.png',
             'type': 'image/png',
             'sizes': '144x144'
         },
         {
-            'src': path.resolve('src/assets/img/favicon-color-192.png'),
+            'src': '/assets/img/favicon-color-192.png',
             'type': 'image/png',
             'sizes': '192x192'
         },
         {
-            'src': path.resolve('src/assets/img/favicon-color-512.png'),
+            'src': '/assets/img/favicon-color-512.png',
             'type': 'image/png',
             'sizes': '512x512'
         }
